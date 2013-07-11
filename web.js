5445a5de0faa7fe92b50cba90744446985312690
Ubuntu <ubuntu@ip-172-31-20-63.us-west-2.compute.internal>
2013-07-11 18:26:10 +0000

    added index.html

--- web.js
+++ web.js
@@ -1,12 +1,16 @@
 var express = require('express');
+var fs = require('fs');
+var content = new Buffer (fs.readFileSync("index.html"), "utf-8");
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(content.toString("utf-8",0,999999999999));
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+    
